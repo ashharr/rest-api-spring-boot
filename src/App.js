0d57c1ea1838915818bac6ef553d75d25ca53ae5
ashharr <shaikh.ashhar6@gmail.com>
2023-07-10 18:28:49 +0530

    react front end for todo app with auth

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Counter from './components/counter/Counter';
+import TodoAppComponent from './components/todo/TodoAppComponent';
+
+function App() {
+  return (
+    <div className="App">
+      
+      {/*       
+      <LearningComponent/>
+      <LearningJavaScript/> 
+      */}
+      {/* <Counter/> */}
+
+      <TodoAppComponent/>
+
+    </div>
+  );
+}
+
+// Function Component
+// Class component that extends Component from 'react'
+
+
+
+export default App;
